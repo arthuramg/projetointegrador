64951af10f37730e307095bb2ab1bae8b7766433
arthurdev <arthurmares44@gmail.com>
2024-11-17 22:52:45 -0300

    Adicionando modelo de resposta do servidor

---
+++ src/main/java/com/arthur/projeto_integrador/models/RespostaServer.java
@@ -0,0 +1,13 @@
+package com.arthur.projeto_integrador.models;
+
+public class RespostaServer {
+    private String mensagem;
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+}
